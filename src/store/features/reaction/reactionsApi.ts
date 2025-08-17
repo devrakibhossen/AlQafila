f6f2851319d3043a31b0d8bb6d250ef22863ff12
Rakib <hellorakibhossen@gmail.com>
2025-08-17 17:31:38 +0600

    updated reactionapi file

--- src/store/features/reaction/reactionsApi.ts
+++ src/store/features/reaction/reactionsApi.ts
@@ -1,4 +1,23 @@
-export const addReactions = async (data) => {
+export interface Reaction {
+  _id?: string;
+  postId: string;
+  userId:
+    | string
+    | {
+        _id: string;
+        username: string;
+        name: string;
+        profileImage: string;
+      };
+  type: string | null;
+  createdAt?: string;
+  updatedAt?: string;
+}
+export interface ReactionId {
+  _id?: string;
+}
+
+export const addReactions = async (data: Reaction) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/reaction`,
     {
@@ -12,7 +31,7 @@ export const addReactions = async (data) => {
   return response.json();
 };
 
-export const getReaction = async (id) => {
+export const getReaction = async (id: ReactionId) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/reaction/${id}`,
     { cache: "no-store" }
