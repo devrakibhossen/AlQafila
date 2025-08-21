1ae39ee167ba9d1916dd90a9be9724839c728c36
Rakib <hellorakibhossen@gmail.com>
2025-08-21 20:00:00 +0600

    post details page updated

--- src/app/post/[id]/[slug]/page.tsx
+++ src/app/post/[id]/[slug]/page.tsx
@@ -4,30 +4,30 @@ export const metadata: Metadata = {
   title: "AlQafila | Post Deails",
 };
 
-
-interface ParamsProps {
+interface PageProps {
   params: {
     id: string;
     slug: string;
   };
 }
-const page = async({params}:ParamsProps) => {
+const page = async ({ params }: PageProps) => {
+  const { id, slug } = params;
 
-  const {id , slug } = params ;
-
-  console.log("This is params: " , id, slug);
+  console.log("This is params: ", id, slug);
   let data = null;
-  try{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts/${id}/${slug}`);
-   data = await res.json();
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts/${id}/${slug}`
+    );
+    data = await res.json();
     console.log("This sis data", data);
-  }catch (err) {
+  } catch (err) {
     console.log(err);
   }
 
   return (
     <div className="w-11/12 mx-auto">
-      <PostDetails post={data?.data}/>
+      <PostDetails post={data?.data} />
     </div>
   );
 };
