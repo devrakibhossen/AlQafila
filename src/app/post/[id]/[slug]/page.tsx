40e5827c15de3a71fab4c441bfe2eee6df119d99
Rakib <hellorakibhossen@gmail.com>
2025-08-21 20:05:15 +0600

    post details page updated

--- src/app/post/[id]/[slug]/page.tsx
+++ src/app/post/[id]/[slug]/page.tsx
@@ -1,26 +1,60 @@
+// import PostDetails from "../../components/PostDetails";
+// import { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "AlQafila | Post Deails",
+// };
+
+// interface PageProps {
+//   params: {
+//     id: string;
+//     slug: string;
+//   };
+// }
+// const page = async ({ params }: PageProps) => {
+//   const { id, slug } = params;
+
+//   console.log("This is params: ", id, slug);
+//   let data = null;
+//   try {
+//     const res = await fetch(
+//       `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts/${id}/${slug}`
+//     );
+//     data = await res.json();
+//     console.log("This sis data", data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+
+//   return (
+//     <div className="w-11/12 mx-auto">
+//       <PostDetails post={data?.data} />
+//     </div>
+//   );
+// };
+
+// export default page;
+
+
 import PostDetails from "../../components/PostDetails";
 import { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: "AlQafila | Post Deails",
+  title: "AlQafila | Post Details",
 };
 
-interface PageProps {
-  params: {
-    id: string;
-    slug: string;
-  };
-}
-const page = async ({ params }: PageProps) => {
+const Page = async ({
+  params,
+}: {
+  params: { id: string; slug: string };
+}) => {
   const { id, slug } = params;
 
-  console.log("This is params: ", id, slug);
   let data = null;
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts/${id}/${slug}`
     );
     data = await res.json();
-    console.log("This sis data", data);
   } catch (err) {
     console.log(err);
   }
@@ -32,4 +66,4 @@ const page = async ({ params }: PageProps) => {
   );
 };
 
-export default page;
+export default Page;
